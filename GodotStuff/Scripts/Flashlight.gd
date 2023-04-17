c6441fe6c882e9cae3d35d9f9223c1ff23d750de
Adam Smith <adamjsmith002@gmail.com>
2023-04-17 03:03:04 -0400

    Flashlight Mechanics + Sounds
    - Added an on/off component to the flashlight
    - Added some sound effects for light and background
    - Basic logic for using a timer to deal with battery level... displayed on console for now
    - Created a file with links to the sound assets I use so its easier to track them down later if we need to credit them.. file named sourceLinks.gd

--- GodotStuff/Scripts/Flashlight.gd
+++ GodotStuff/Scripts/Flashlight.gd
@@ -1,8 +1,41 @@
 extends Area2D
 
+func _ready():
+	$batteryCountdown.start(100)
+	
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		set_global_position(event.position)
+	#in theory, clicking should go here too but
+	#it continuously turns back on.. so i stuffed it
+	#in _process for now
+		
+func _process(delta):
+	#for dealing with turning light on and off
+	if Input.is_action_just_released("leftClick"):
+		lightAudioHandler(lightStatus())
+		visible = false if lightStatus() else true
+		print("click!")
+	#for implementing simple flashlight battery
+	batteryHandler()
+
+func lightStatus():
+	return visible;
+	
+func lightAudioHandler(status):
+	if status == true:
+		$on.play()
+	else:
+		$off.play()
+
+func batteryHandler(): 
+	if lightStatus() == false:
+		$batteryCountdown.set_paused(true)
+	else:
+		$batteryCountdown.set_paused(false)
+	print("Battery Left: " + str($batteryCountdown.time_left))
+
 
 func litAreaEntered(area):
 	if area.has_method("spotted"):
@@ -11,3 +44,4 @@ func litAreaEntered(area):
 func litAreaExited(area):
 	if area.has_method("unspotted"):
 		area.unspotted()
+		
---
+++ GodotStuff/resourceLinks.gd
@@ -0,0 +1,6 @@
+extends Node
+
+#Light switch sounds: https://www.youtube.com/watch?v=gOLB0kWxJak&ab_channel=FreeSoundEffects
+
+#Forest Ambience: https://www.youtube.com/watch?v=n_sEDCaS9zU&ab_channel=Lost%26Sound-Topic
+
