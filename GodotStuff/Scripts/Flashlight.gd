196fd3f49637feb35e88f77623653b75ffd36f6c
Adam Smith <adamjsmith002@gmail.com>
2023-04-17 17:08:41 -0400

    more flashlight logic
    - turns off at zero percent
    - charging logic
    - groovy charging sound
    - Some bugs to work out with turning on and off/ registering something as seen

--- GodotStuff/Scripts/Flashlight.gd
+++ GodotStuff/Scripts/Flashlight.gd
@@ -3,6 +3,9 @@ extends Area2D
 var isLightOn: bool = false
 @onready var percentIndicator = $"../Battery Remaining"
 
+var pauseBattery = false;
+
+var charging = false;
 
 func _ready():
 	$BatteryTimer.start(100)
@@ -11,21 +14,23 @@ func _ready():
 func _process(_delta):
 	percentIndicator.text = str(roundi($BatteryTimer.time_left))+"%"
 
+	chargeHandler();
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		set_global_position(event.position)
 	elif event is InputEventMouseButton:
 		if event.button_index == 1 && event.pressed == true:
 			switchFlashlight()
-		
 func switchFlashlight():
-	lightAudioHandler()
-	isLightOn = !isLightOn
-	visible = isLightOn
-	for area in self.get_overlapping_areas():
-		if area.is_in_group("Monster") && isLightOn:
-			area.spotted();
-	batteryHandler()
+	if !pauseBattery:
+		lightAudioHandler()
+		isLightOn = !isLightOn
+		visible = isLightOn
+		for area in self.get_overlapping_areas():
+			if area.is_in_group("Monster") && isLightOn:
+				area.spotted();
+		batteryHandler()
 
 func lightAudioHandler():
 	if isLightOn:
@@ -46,3 +51,21 @@ func litAreaExited(area):
 	if area.has_method("unspotted") && isLightOn:
 		area.unspotted()
 		
+
+func _on_battery_timer_timeout():
+	visible = false;
+	pauseBattery = true;
+	
+#handle charging
+func chargeHandler():
+	if Input.is_action_just_pressed("ui_accept") && $BatteryTimer.time_left < 90:
+		print("charging...")
+		$chargingSound.play()
+		var currentBattery = $BatteryTimer.get_time_left()
+		$BatteryTimer.set_paused(true)
+		pauseBattery = true
+		visible = false
+		await get_tree().create_timer(10).timeout
+		$BatteryTimer.start(currentBattery + 10)
+		print($BatteryTimer.time_left)
+		pauseBattery = false
