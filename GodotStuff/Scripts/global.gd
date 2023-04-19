321d2aa3bd6ca1854818319c99b0766909990de9
ProphecyOak <drito610@gmail.com>
2023-04-18 23:02:21 -0400

    Add Win Condition (Timer)

--- GodotStuff/Scripts/global.gd
+++ GodotStuff/Scripts/global.gd
@@ -7,10 +7,13 @@ var gameOver = false;
 var cribToggleKey = 32 #Spacebar
 var chargeKey = 67 #c
 
+var timeTillWin = 500
+var won = false
+
 func toggleSounds():
 	isAudioOn = !isAudioOn
 	print("Sounds on: "+str(isAudioOn))
-	
+
 func toggleJumpScares():
 	isJumpScaresOn = !isJumpScaresOn
 	print("Jump Scares on: "+str(isJumpScaresOn))
