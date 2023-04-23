bef3111c97b06e4207ec41dc597c7ea35d763781
ProphecyOak <drito610@gmail.com>
2023-04-23 12:47:19 -0400

    Remove Excess Process functions

--- GodotStuff/Scenes/win_cutscene.gd
+++ GodotStuff/Scenes/win_cutscene.gd
@@ -7,18 +7,9 @@ func _ready():
 	$Camera2D.zoom.x = 1;
 	$Camera2D.zoom.y = 1;
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_light_on_finished():
 	$footSteps.play()
 	$AnimationPlayer.play("walkingUp")
-
-
-
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "walkingUp":
